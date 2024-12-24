b331983e433d597b48330867ebb306066a21092a
Aditya 9570 <aditya8prakash@gmail.com>
2024-12-24 09:19:03 +0530

    this  my first java script

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log("hello")
+// let obj = {
+//     Naam:"sam"
+//     Age :22
+//     Role :"teacher"
+
+// }
+// for(let item in obj){
+// console.log(item , obj[j])
+// }
+function aditya()
+{
+ let i=5
+ for (let i = 0; i < 40; i++)
+  
+    console.log(i);
+    
+}
+aditya()
+aditya()
